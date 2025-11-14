4a3a625a9c06963b1c2904fa89e97f7bd40d60df
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-14 23:16:04 +0530

    `just addd`

--- plot-2d.js
+++ plot-2d.js
@@ -1,24 +1,25 @@
 // const barLen = 3;
 const DIMENSION = 20;
 // swirl in 2d rotate and reach center 
-const array = [1, 1, 15, 13, 4, 5];
 const CELL = ' ';
+const array = [1, 2, 3];
 const graph = () => {
   const grid = [];
-
+  const row = CELL.repeat(DIMENSION);
   for (let column = 0; column < DIMENSION; column++) {
-    const row = CELL.repeat(DIMENSION).split(''); //value is column count and index is row ;
-    grid.push(row);
+    const cells = row.split(''); //value is column count and index is row ;
+    grid.push(cells);
   }
+
   let offset = 0;
   for (let rowPos = 0; rowPos < array.length; rowPos += 1) {
     const barLen = array[rowPos];
 
     for (let column = 0; column < barLen; column += 1) {
       const currentColumn = column + (DIMENSION - barLen);
-      console.log(currentColumn, currentColumn + 1);
+      // console.log(currentColumn, currentColumn + 1);
       grid[currentColumn][rowPos + offset] = '|';
-      grid[currentColumn][rowPos + 1 + offset] = '|';
+      grid[currentColumn][rowPos + 2 + offset] = '|';
     }
     offset += 2;
 
