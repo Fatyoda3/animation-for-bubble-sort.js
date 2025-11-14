043fa1e3771b1c7feda49aec50a8c973ac321a7c
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-14 23:43:34 +0530

    `just push I hate my life `

--- plot-2d.js
+++ plot-2d.js
@@ -1,4 +1,4 @@
-const DIMENSION = 20;
+const DIMENSION = 50;
 // swirl in 2d rotate and reach center 
 const CELL = ' ';
 
@@ -11,27 +11,27 @@ const plotBarGraph = (barWeights) => {
     grid.push(cells);
   }
 
-  let columnOffset = 0;
+  let spaceBetweenBar = 0;
+
   for (let colPos = 0; colPos < barWeights.length; colPos += 1) {
     const barWeight = barWeights[colPos];
-    const rowOffset = (DIMENSION - barWeight);
-
+    const rowOffset = DIMENSION - barWeight;
+    const peakX = rowOffset - 1;
     for (let row = 0; row < barWeight; row += 1) {
-
       const x = row + rowOffset;
+      const y = colPos + spaceBetweenBar;
+      console.log(x, y);
 
-      const y = colPos + columnOffset;
-      grid[rowOffset - 1][y + 1] = '_';
+      grid[peakX][y + 1] = '_';
       grid[x][y] = '|';
       grid[x][y + 2] = '|';
     }
 
-    columnOffset += 2;
-
+    spaceBetweenBar += 2;
   };
 
   return grid.map(row => row.join('')).join('\n');
 };
 
-const array = [1, 2, 3, 9, 3, 2];
+const array = [1, 2, 3, 9, 3, 2, 4];
 console.log(plotBarGraph(array));;
